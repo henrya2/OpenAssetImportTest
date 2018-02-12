65dd6e6692965e27c66f5496c952b422960364af
Henry Read <henrya2@126.com>
2018-02-12 18:17:40 +0800

    initial commits ....

---
+++ OpenAssetImport/Source/OpenAssetImport/OpenAssetImportGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenAssetImportGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENASSETIMPORT_API AOpenAssetImportGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
